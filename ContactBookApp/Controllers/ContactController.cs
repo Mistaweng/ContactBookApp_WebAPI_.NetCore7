7595b102a741a7b46f0beb604857cd3f590827c7
Mistaweng <emmanuelgyang.1822@gmail.com>
2023-07-25 12:15:43 +0100

    Add project files.

---
+++ ContactBookApp/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using ContactBookApp.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactBookApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult<IEnumerable<ContactDto>> GetContacts()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ContactBookApp/Database/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactBookApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactBookApp.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Contact> Contacts { get; set; }
+    }
+}
